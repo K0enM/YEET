0db4a1915ac52fe391ae2ff109cb13a2ee865dd9
Koen Molenaar <k.molenaar@student.utwente.nl>
2020-04-08 20:01:43 +0200

    Implement openall and own regex

--- shell.py
+++ shell.py
@@ -1,32 +1,56 @@
 import cmd
 import requests
 import re
+import webbrowser
 
 
 class Terminal(cmd.Cmd):
-    intro = 'Welcome to the molenaar shell. Type help or ? to list commands'
+    intro = "Welcome to the molenaar shell. Type help or ? to list commands"
     prompt = '[$] '
+    links =[]
 
     def do_search(self, url):
         """search [url]
-        search the given url for youtube links"""
+        Searches the given url for youtube links"""
         if url:
-            prog = re.compile('^(http|https)://')
-            if prog.match(url):
+            if re.match(r"^(http|https)://", url):
                 text = requests.get(url).text
-                matches = re.finditer(
-                    r"(?:https?://)?(?:www\.)?youtu\.?be(?:\.com)?/?.*(?:watch|embed)?(?:.*v=|v/|/)([\w-]+)", text,
-                    re.MULTILINE | re.IGNORECASE)
+                # matches = re.finditer(
+                #     r"(?:https?://)?(?:www\.)?youtu\.?be(?:\.com)?/?.*(?:watch|embed)?(?:.*v=|v/|/)([\w-]{1,12})", text,
+                #     re.MULTILINE | re.IGNORECASE)
+                pattern = r"(http|https)://(www.youtube.com/watch[?]v=)(.{1,11})"
+                matches = re.finditer(pattern, text)
                 for num, match in enumerate(matches, start=1):
                     print("Match {num} was found at {start}--{end}: {match}".format(num=num, start=match.start(),
-                                                                                    end=match.end(), match=match))
+                                                                                    end=match.end(), match=match.group(0)))
+                    self.links.append(match.group())
             else:
-                print("please enter a valid url starting with http:// or https://")
+                print("Please enter a valid url starting with http:// or https://")
         else:
-            print("please enter a url")
+            print("Please enter a url")
+
+    def do_open(self, url):
+        """open [url]
+        Opens the given url in the default browser"""
+        if url:
+            if re.match(r"^(http|https)://", url):
+                webbrowser.open(url, new=2)
+            else:
+                print("Please enter a valid url starting with http:// or https://")
+        else:
+            print("Please enter a url")
+
+    def do_openall(self, arg):
+        """Opens all youtube links found by the search command. Empties stored urls after."""
+        if len(self.links) == 0:
+            print("No stored links to open")
+            return
+        for link in self.links:
+            webbrowser.open(link, new=2)
+            self.links.remove(link)
 
     def do_EOF(self, arg):
-        """quits the shell"""
+        """Quits the shell"""
         return True
 
 
