ad9a497c172a224f27f518db139ca3de396f9c6d
Koen Molenaar <k.molenaar@student.utwente.nl>
2020-04-09 02:28:02 +0200

    Start work on colored messages

--- shell.py
+++ shell.py
@@ -2,12 +2,14 @@ import cmd
 import requests
 import re
 import webbrowser
+from termcolor import colored
+from colorama import Fore, Style
 
 
 class Terminal(cmd.Cmd):
     intro = "Welcome to the molenaar shell. Type help or ? to list commands"
     prompt = '[$] '
-    links =[]
+    links = []
 
     def do_search(self, url):
         """search [url]
@@ -15,19 +17,17 @@ class Terminal(cmd.Cmd):
         if url:
             if re.match(r"^(http|https)://", url):
                 text = requests.get(url).text
-                # matches = re.finditer(
-                #     r"(?:https?://)?(?:www\.)?youtu\.?be(?:\.com)?/?.*(?:watch|embed)?(?:.*v=|v/|/)([\w-]{1,12})", text,
-                #     re.MULTILINE | re.IGNORECASE)
-                pattern = r"(http|https)://(www.youtube.com/watch[?]v=)(.{1,11})"
-                matches = re.finditer(pattern, text)
+                pattern = r"(http|https)://(www\.)?(youtube.com|youtu.be)/(watch)?(\?v=)?(\S+)?/"
+                matches = re.finditer(pattern, text, re.MULTILINE | re.IGNORECASE)
                 for num, match in enumerate(matches, start=1):
-                    print("Match {num} was found at {start}--{end}: {match}".format(num=num, start=match.start(),
-                                                                                    end=match.end(), match=match.group(0)))
+                    print(colored("Match {num} was found at {start}--{end}: {match}".format(num=num, start=match.start(),
+                                                                                    end=match.end(),
+                                                                                    match=match.group(0)), "green", attrs=["bold"]))
                     self.links.append(match.group())
             else:
-                print("Please enter a valid url starting with http:// or https://")
+                print(colored("Please enter a valid url starting with http:// or https://", "red", attrs=["bold"]))
         else:
-            print("Please enter a url")
+            print(colored("Please enter a url", "red", attrs=["bold"]))
 
     def do_open(self, url):
         """open [url]
@@ -36,14 +36,14 @@ class Terminal(cmd.Cmd):
             if re.match(r"^(http|https)://", url):
                 webbrowser.open(url, new=2)
             else:
-                print("Please enter a valid url starting with http:// or https://")
+                print(colored("Please enter a valid url starting with http:// or https://", "red", attrs=["bold"]))
         else:
-            print("Please enter a url")
+            print(colored("Please enter a url", "red", attrs=["bold"]))
 
     def do_openall(self, arg):
         """Opens all youtube links found by the search command. Empties stored urls after."""
         if len(self.links) == 0:
-            print("No stored links to open")
+            print(colored("No links to open", "red", attrs=["bold"]))
             return
         for link in self.links:
             webbrowser.open(link, new=2)
@@ -51,6 +51,7 @@ class Terminal(cmd.Cmd):
 
     def do_EOF(self, arg):
         """Quits the shell"""
+        print(f"{Fore.RED}Goodbye{Style.RESET_ALL}")
         return True
 
 
